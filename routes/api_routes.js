1841d3c35eb8debbfd159614419c3170298c3f0a
Troy Dorman <troydorman@gmail.com>
2021-02-15 15:31:30 -0700

    refactored the api routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,15 +1,17 @@
+const express = require('express')
+const router = express.Router()
 const db = require('../models')
 
-module.exports = function (app) {
-  app.get('/api/images', function (req, res) {
-    db.Image.find({}).then(function (dbImages) {
-      res.json(dbImages)
-    })
+router.get('/api/images', function (req, res) {
+  db.Image.find({}).then(function (dbImages) {
+    res.json(dbImages)
   })
+})
 
-  app.put('/api/images/:id', function (req, res) {
-    db.Image.updateOne({ _id: req.params.id }, { rating: req.body.rating }).then(function (dbImage) {
-      res.json(dbImage)
-    })
+router.put('/api/images/:id', function (req, res) {
+  db.Image.updateOne({ _id: req.params.id }, { rating: req.body.rating }).then(function (dbImage) {
+    res.json(dbImage)
   })
-}
+})
+
+module.exports = router
